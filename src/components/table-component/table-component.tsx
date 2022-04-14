16949063b3c0ba84c7ee9e9d1f2409442c067f8d
slavenr <slaven.ranisavljevic@amplitudo.me>
2022-04-14 12:32:10 +0200

    completes checkbox design and adds functionalities for design testing

--- src/components/table-component/table-component.tsx
+++ src/components/table-component/table-component.tsx
@@ -1,4 +1,4 @@
-import { h, Component, Host, Prop, State } from "@stencil/core";
+import { h, Component, Host, Prop, State, Watch } from "@stencil/core";
 import LeftArrow from "../../assets/images/left-arrow.svg";
 import RightArrow from "../../assets/images/right-arrow.svg";
 import NoSort from "../../assets/images/no-sort.svg";
@@ -15,9 +15,22 @@ export interface Column {
 })
 export class TableComponent {
 
+  checkboxRef!: HTMLInputElement;
+
   @Prop({ mutable: true }) columns: Array<Column>;
   @Prop({ mutable: true }) dataSet: Array<Object>;
   @Prop() dataApi?: String;
+
+  @State() checkedEntries: Array<any> = [];
+
+  @Watch('checkedEntries')
+  checkedEntriesHandler(currValue: Array<any>) {
+    if (currValue.length > 0) {
+      this.checkboxRef.indeterminate = true;
+    } else {
+      this.checkboxRef.indeterminate = false;
+    }
+  }
 
   render() {
     return (
@@ -25,7 +38,7 @@ export class TableComponent {
         <table>
           <thead>
             <tr>
-              <th><div class="checkbox"><input type="checkbox" /></div></th>
+              <th><div class="checkbox"><input ref={(el) => this.checkboxRef = el as HTMLInputElement} type="checkbox" /></div></th>
               {
                 this.columns.map((column) => {
                   return <th key={column.label}><div>{column.label} <img src={NoSort} alt='leftArrow' /></div></th>
@@ -36,8 +49,13 @@ export class TableComponent {
           <tbody>
             {
               this.dataSet.map((data) => {
-                return <tr key={data['CustomerId']}>
-                  <td><div class="checkbox"><input type="checkbox" /></div></td>
+                return <tr key={data['CustomerID']}>
+                  <td><div class="checkbox"><input type="checkbox" onClick={(e) => {
+                    (e.target as HTMLInputElement).checked ?
+                      this.checkedEntries = [...this.checkedEntries, data['CustomerID']]
+                      :
+                      this.checkedEntries = [...this.checkedEntries.filter((entry) => entry !== data['CustomerID'])];
+                  }} /></div></td>
                   {
                     this.columns.map((column) => {
                       return <td key={column.label} title={data[column.dataField]}>{data[column.dataField]}</td>
